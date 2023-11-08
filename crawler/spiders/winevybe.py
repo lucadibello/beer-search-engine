a9241725cbc74364792cf7d84f6927bb58cf8907
Luca Di Bello <info@lucadibello.ch>
2023-11-08 17:19:52 +0100

    Winevybe scraper works perfectly

--- crawler/spiders/winevybe.py
+++ crawler/spiders/winevybe.py
@@ -6,13 +6,24 @@ class WinevybeSpider(scrapy.Spider):
     name = "winevybe"
     allowed_domains = ["winevybe.com"]
 
+    _all_scraped = False
+
     def start_requests(self):
-        for i in range(1):
+        # Scrape all pages!
+        index = 1
+        while not self._all_scraped:
             yield scrapy.Request(
-                url=f"https://winevybe.com/beer/page/{i}/", callback=self.parse
+                url=f"https://winevybe.com/beer/page/{index}/", callback=self.parse
             )
+            # Increment page index
+            index += 1
 
     def parse(self, response):
+        # Check if all pages have been scraped (404 page)
+        if response.status == 404:
+            # Set flag
+            self._all_scraped = True
+
         # Print response
         products = response.css("li.product.type-product")
 
@@ -24,18 +35,87 @@ class WinevybeSpider(scrapy.Spider):
             # Send request
             yield scrapy.Request(url=product_url, callback=self.parse_product)
 
-    def parse_product(self, response):
+    def parse_product(self, response: scrapy.Selector):
+        # Extract brand and name
+        brand, name = self.parse_product_brand_name(response)
+
+        # Extract product image URL
+        image_url = response.css(
+            ".woocommerce-product-gallery__image > img::attr(src)"
+        ).get()
+
+        # Extract product price
+        price, currency = self.parse_product_price(response)
+
+        ## NOW, SCRAPING THE TABLE
+        possible_headers = {
+            "Critic Score": "critic_score",
+            "Producer": "producer",
+            "Type": "type",
+            "Alcohol bv": "alcohol_bv",
+            "Tasting Notes": "tasting_notes",
+            "Closure": "closure",
+            "Packaging": "packaging",
+            "MPN #": "mpn",
+        }
+
+        # Initialize table_data with all possible headers set to None
+        table_data = {header: None for header in possible_headers.values()}
+
+        # Identify table rows
+        rows = response.css(".around")
+
+        # Extract data from column
+        for row in rows:
+            property_name, value = self.parse_table_rows(row)
+
+            # Ensure that data has been found and is a known header
+            if property_name in possible_headers:
+                # If name is Critic Score and value is not None, extract max and actual score
+                if property_name == "Critic Score" and value is not None:
+                    parts = value.split("/")
+                    actual_critics_score = int(parts[0])
+                    max_critics_score = int(parts[1])
+
+                    # Update dictionary for critic_score with a sub-dictionary
+                    table_data[possible_headers[property_name]] = {  # type: ignore
+                        "max": max_critics_score,
+                        "actual": actual_critics_score,
+                    }
+                else:
+                    # Update the value for this header in table_data
+                    table_data[possible_headers[property_name]] = value  # type: ignore
+
+        # Return product
+        yield {
+            "brand": brand,
+            "name": name,
+            "description": self.parse_product_description(response),
+            "image_url": image_url,
+            "price": {
+                "amount": price,
+                "currency": currency,
+            },
+            **table_data,  # Spread dictionary containing row data
+        }
+
+    def parse_table_rows(self, row: scrapy.Selector) -> tuple[str | None, str | None]:
+        # .wineleft = Name
+        # .wineright = Actual value
+        data_name = row.css(".wineleft::text").get()
+        data_value = row.css(".wineright::text").get()
+        return (data_name, data_value)
+
+    def parse_product_brand_name(
+        self, response: scrapy.Selector
+    ) -> tuple[str | None, str]:
         # Extract brand name
-        brand = (
-            response.xpath('//*[@id="ajax-content-wrap"]/div[1]/h1/span/text()')
-            .get()
-            .strip()
-        )
-        name = (
-            response.css("#ajax-content-wrap > div:nth-child(1) > h1::text")
-            .getall()[1]
-            .strip()
-        )
+        brand = response.css(".brand::text").get()
+        name = "".join(response.css(".product::text").getall()).strip()
+
+        # If brand has been found, strip it
+        if brand:
+            brand = brand.strip()
 
         # Check if name is empty
         if not name:
@@ -43,83 +123,54 @@ class WinevybeSpider(scrapy.Spider):
             tmpname = (
                 response.css(".woocommerce-breadcrumb span::text").getall()[-1].strip()
             )
-
             # If name contains brand name, remove it
-            if brand in tmpname:
+            if brand and brand in tmpname:
                 name = tmpname.replace(brand, "").strip()
             else:
                 name = tmpname
+        else:
+            # Strip name
+            name = name.strip()
 
+        return (brand, name)
+
+    def parse_product_description(self, response: scrapy.Selector) -> str:
         # Extract description
-        description_content: list[str] = [
-            p.strip()
-            for p in response.xpath(
-                '//*[starts-with(@id, "product-")]/div[1]/div[1]/div[2]/p[2]/text()'
-            ).getall()
-        ]
-        missing_pieces: list[str] = [
-            p.strip()
-            for p in response.xpath(
-                '//*[starts-with(@id, "product-")]/div[1]/div[1]/div[2]/p[2]/*/text()'
-            ).getall()
-        ]
+        description_pieces = response.css(".short-description > p").getall()
 
-        # After each description element, add a missing piece
-        for i in range(len(missing_pieces)):
-            description_content.insert(i * 2 + 1, missing_pieces[i])
+        # NOTE: Workaround to extract text content from pieces
+        description_content = []
+        for piece in description_pieces:
+            # Extract text content from piece
+            piece_content = scrapy.Selector(text=piece).css("::text").getall()
+            # Append
+            description_content.append("".join(piece_content))
 
         # Join + sanitize description
         description = (
             "".join(description_content)
-            .replace("\n", "")
-            .replace("\t", "")
-            .replace("\r", "")
+            .replace("\n", " ")
+            .replace("\t", " ")
+            .replace("\r", " ")
         )
+
+        # Remove multiple spaces
+        description = re.sub(r"\s+", " ", description)
+
         # Add spaces after dots
-        description = re.sub(r"\.(?! )", ". ", description)
+        description = re.sub(r"\.(?! )", ". ", description).strip()
 
-        ## NOW, SCRAPING THE TABLE
-        possible_headers = [
-            "Critic Score",
-            "Producer",
-            "Type",
-            "Alcohol bv",
-            "Tasting Notes",
-            "Closure",
-            "Packaging",
-            "MPN #",
-        ]
+        # Return description
+        return description
 
-        # For each row of the table, check the header
+    def parse_product_price(
+        self, response: scrapy.Selector
+    ) -> tuple[float | None, str | None]:
+        # Extract price
+        price = response.css(".woocommerce-Price-amount > bdi::text").get()
 
-        # Extract critic score
-        tmp_critic_score = (
-            response.xpath(
-                '//*[starts-with(@id, "product-")]/div[1]/div[1]/div[3]/div[1]/span[3]/text()'
-            )
-            .get()
-            .strip()
-            .split("/")
-        )
-        max_critics_score = int(tmp_critic_score[1])
-        actual_critics_score = int(tmp_critic_score[0])
+        # Extract currency
+        currency = response.css(".woocommerce-Price-currencySymbol::text").get()
 
-        # Extract closure type
-        closure_type = response.xpath(
-            '//*[starts-with(@id, "product-")]/div[1]/div[1]/div[3]/div[3]/span[3]/text()'
-        ).get()
-
-        # Extract MPN
-        mpn = response.xpath(
-            '//*[starts-with(@id, "product-")]/div[1]/div[1]/div[3]/div[4]/span[3]/text()'
-        ).get()
-
-        # Return product
-        yield {
-            "brand": brand,
-            "name": name,
-            "description": description,
-            "critic_score": {"max": max_critics_score, "actual": actual_critics_score},
-            "closure_type": closure_type,
-            "mpn": mpn,
-        }
+        # Return tuple
+        return (float(price) if price is not None else None, currency)
