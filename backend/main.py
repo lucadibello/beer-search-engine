1d0c9aae4db8c6464c576514073dbd075d369688
Luca Di Bello <lucadibello@proton.me>
2023-11-24 13:05:36 +0100

    Many updates and fixes

--- backend/main.py
+++ backend/main.py
@@ -4,33 +4,49 @@ import pandas as pd
 from fastapi import FastAPI
 import pyterrier as pt
 
+if not pt.started():
+    pt.init()
+
 from modules.indexer import Indexer
+
+# Constants
+DATASET_PATH = "../data/data.jsonl"
+INDEX_PATH = "./.beer-index"
+API_VERSION = "1"
 
 
 def load_documents(path: str):
     # Load the documents from the dataset
     docs = Indexer.load_dataset(path)
 
-    # Spread nested dicts into the main dict
-    docs = [pd.json_normalize(doc) for doc in docs]
-    
-    
+    # Load documents inside dataframe
+    df = pd.DataFrame(docs)
+
+    # Use column "docno" as index
+    df.set_index("docno")
 
     # Return dataframe
     return df
 
 
 # Load dataframe
-df = load_documents("../data/data.jsonl")
+df = load_documents(DATASET_PATH)
+
+# Load index and build retrieval model
+index = pt.IndexFactory.of(INDEX_PATH)
+model = pt.BatchRetrieve(index, wmodel="BM25")
 
 app = FastAPI()
 
-# TODO: Load documents from dataset + load index and build retrival modal using PyTerrier
 
+@app.get("/api/v" + API_VERSION + "/search")
+def search(query: str, top: int = 10):
+    # Search query
+    results = index.search(query)
+    print(results)
 
-@app.get("/")
-def read_root():
-    return df.iloc[0].to_dict()
+    # Return results
+    return results.head(top)
 
 
 @app.get("/items/{item_id}")
