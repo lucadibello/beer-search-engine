3e1ecd22d2b264826ea89080a3bcd11a31db974f
Luca Di Bello <info@lucadibello.ch>
2023-11-27 10:28:24 +0000

    Fixed navbar component

--- beer-search-engine/components/Nav.tsx
+++ beer-search-engine/components/Nav.tsx
@@ -3,19 +3,10 @@
 import {
   Box,
   Flex,
-  Avatar,
-  Text,
   Button,
-  Menu,
-  MenuButton,
-  MenuList,
-  MenuItem,
-  MenuDivider,
-  useDisclosure,
   useColorModeValue,
   Stack,
   useColorMode,
-  Center,
   Icon,
 } from '@chakra-ui/react'
 import { FiMoon, FiSun } from 'react-icons/fi'
@@ -24,28 +15,8 @@ interface Props {
   children: React.ReactNode
 }
 
-const NavLink = (props: Props) => {
-  const { children } = props
-
-  return (
-    <Box
-      as="a"
-      px={2}
-      py={1}
-      rounded={'md'}
-      _hover={{
-        textDecoration: 'none',
-        bg: useColorModeValue('gray.200', 'gray.700'),
-      }}
-      href={'#'}>
-      {children}
-    </Box>
-  )
-}
-
 export default function Nav() {
   const { colorMode, toggleColorMode } = useColorMode()
-  const { isOpen, onOpen, onClose } = useDisclosure()
   return (
     <>
       <Box bg={useColorModeValue('gray.100', 'gray.900')} px={4}>
