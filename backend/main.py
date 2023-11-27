416fc1efcb14969dde04980b1cc6f8551562bd4c
Luca Di Bello <info@lucadibello.ch>
2023-11-27 10:27:48 +0000

    Implemented search API

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,9 @@
+from pydoc import doc
 from typing import Union
 
 import pandas as pd
 from fastapi import FastAPI
+from fastapi.responses import ORJSONResponse
 import pyterrier as pt
 
 if not pt.started():
@@ -25,6 +27,10 @@ def load_documents(path: str):
     # Use column "docno" as index
     df.set_index("docno")
 
+    # Count nan / null values
+    print(df.dtypes)
+    print(df.isnull().sum())
+
     # Return dataframe
     return df
 
@@ -35,18 +41,33 @@ df = load_documents(DATASET_PATH)
 # Load index and build retrieval model
 index = pt.IndexFactory.of(INDEX_PATH)
 model = pt.BatchRetrieve(index, wmodel="BM25")
+app = FastAPI(default_response_class=ORJSONResponse)
 
-app = FastAPI()
 
-
+# Example URL: /api/v1/search?query=beer&top=10
 @app.get("/api/v" + API_VERSION + "/search")
-def search(query: str, top: int = 10):
-    # Search query
-    results = index.search(query)
-    print(results)
+def search(query: str, top: int = 100):
+    # Search documents by query
+    results = model.search(query)
 
-    # Return results
-    return results.head(top)
+    # Retrieve the document ids from the results
+    ids = results["docno"].tolist()
+
+    # Select only the top results
+    top = min(top, len(ids))
+    ids = ids[:top]
+
+    # Create a DataFrame for ordering
+    order_df = pd.DataFrame({"docno": ids, "order": range(len(ids))})
+
+    # Merge with the original DataFrame
+    merged_docs = order_df.merge(df, on="docno").sort_values(by="order")
+
+    # Drop the temporary ordering column and reset index
+    ordered_docs = merged_docs.drop(columns=["order"]).reset_index(drop=True)
+
+    # Return the documents as JSON
+    return ordered_docs.to_dict("records")
 
 
 @app.get("/items/{item_id}")
