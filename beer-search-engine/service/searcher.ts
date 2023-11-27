3e5838b81c04727ce2d8aef943f4e44bb488965d
Luca Di Bello <info@lucadibello.ch>
2023-11-27 10:28:16 +0000

    Renamed service

--- beer-search-engine/service/searcher.ts
+++
@@ -1,8 +0,0 @@
-const searchBeer = (query: string) => {
-  if (process.env.API_URL === undefined) {
-    throw new Error("API_URL is not defined")
-  }
-  return fetch(process.env.API_URL + "/search?q=" + query)
-}
-
-export {}
