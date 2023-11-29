f43724b9def94e087c108c082319984a2ed97537
Luca Di Bello <info@lucadibello.ch>
2023-11-29 16:52:07 +0000

    Added CORS middleware + now ignoring type errors

--- backend/main.py
+++ backend/main.py
@@ -4,12 +4,14 @@ from typing import Union
 import pandas as pd
 from fastapi import FastAPI
 from fastapi.responses import ORJSONResponse
+from fastapi.middleware.cors import CORSMiddleware
 import pyterrier as pt
 
 if not pt.started():
     pt.init()
 
 from modules.indexer import Indexer
+from modules.sanitizer import sanitize_query
 
 # Constants
 DATASET_PATH = "../data/data.jsonl"
@@ -39,16 +41,28 @@ def load_documents(path: str):
 df = load_documents(DATASET_PATH)
 
 # Load index and build retrieval model
-index = pt.IndexFactory.of(INDEX_PATH)
-model = pt.BatchRetrieve(index, wmodel="BM25")
+index = pt.IndexFactory.of(INDEX_PATH)  # type: ignore
+model = pt.BatchRetrieve(index, wmodel="BM25")  # type: ignore
+
+# Create FastAPI app + CORS middleware
 app = FastAPI(default_response_class=ORJSONResponse)
+origins = [
+    "http://localhost:3000",
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["GET"],
+    allow_headers=["*"],
+)
 
 
 # Example URL: /api/v1/search?query=beer&top=10
 @app.get("/api/v" + API_VERSION + "/search")
 def search(query: str, top: int = 100):
     # Search documents by query
-    results = model.search(query)
+    results = model.search(sanitize_query(query))
 
     # Retrieve the document ids from the results
     ids = results["docno"].tolist()
@@ -68,8 +82,3 @@ def search(query: str, top: int = 100):
 
     # Return the documents as JSON
     return ordered_docs.to_dict("records")
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
