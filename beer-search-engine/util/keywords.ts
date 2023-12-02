9261e5af5c2e03f58da10e2778f6eaec112feb77
Luca Di Bello <lucadibello@proton.me>
2023-12-02 17:57:21 +0000

    Removed special characters from keywords

--- beer-search-engine/util/keywords.ts
+++ beer-search-engine/util/keywords.ts
@@ -8,6 +8,11 @@ const getKeywords = (query: string, minLength: number = 3): string[] => {
   // Remove short words
   const filteredKeywords = keywords.filter(word => word.length >= minLength)
 
+  // Remove special characters from keywords
+  filteredKeywords.forEach((keyword, index) => {
+    filteredKeywords[index] = keyword.replace(/[^a-zA-Z0-9]/g, "")
+  })
+
   // Return keywords
   return filteredKeywords
 }
