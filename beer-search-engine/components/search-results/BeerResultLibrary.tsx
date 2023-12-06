9d1d2f246dd10baca4c33abf56f98cff137d9815
Luca Di Bello <info@lucadibello.ch>
2023-12-06 14:34:00 +0000

    Implemented sorting

--- beer-search-engine/components/search-results/BeerResultLibrary.tsx
+++ beer-search-engine/components/search-results/BeerResultLibrary.tsx
@@ -1,8 +1,11 @@
 "use client"
 
 import { Beer } from "@/service/beer-service"
-import { Box, Heading } from "@chakra-ui/react"
+import { Box, HStack, Heading } from "@chakra-ui/react"
 import { BeerResultStack } from "./BeerResultStack"
+import { useEffect, useState } from "react"
+import { BeerSortingTarget, SortOrder, sortBeers } from "@/util/sorter"
+import SortingSelect from "./SortingSelect"
 
 type BeerResultLibrary = {
   totalHits?: number
@@ -17,17 +20,61 @@ export default function BeerResultLibrary({
   totalHits,
   onBeerSelected,
 }: BeerResultLibrary) {
+  // Save beers in state
+  const [localBeers, setLocalBeers] = useState<Beer[]>(beers)
+
+  const [dirtyFilter, setDirtyFilter] = useState<boolean>(false)
+  const [sorting, setSorting] = useState<BeerSortingTarget | "relevance">(
+    "relevance",
+  )
+  const [order, setOrder] = useState<SortOrder>("descending")
+
+  useEffect(() => {
+    console.log("beers changed", beers)
+  }, [beers])
+
+  useEffect(() => {
+    // If no filter, do nothing
+    if (sorting === "relevance") {
+      // Update only if filter is dirty
+      if (dirtyFilter) {
+        console.log("resetting beers", beers)
+        setLocalBeers(beers)
+        setDirtyFilter(false)
+      }
+    } else {
+      console.log("sorting", sorting, order, beers)
+
+      // Filter beers
+      const filteredBeers = sortBeers(beers, sorting, order)
+
+      // Filter + update local beers
+      setLocalBeers(filteredBeers)
+
+      // Set dirty filter to true if it is not already
+      if (!dirtyFilter) {
+        setDirtyFilter(true)
+      }
+    }
+  }, [beers, sorting, order, dirtyFilter])
+
   return (
     <Box w="100%" px={5}>
-      <Heading size="md" mb={5}>
-        Results:
-        {totalHits && (
-          <span style={{ marginLeft: "10px" }}>{totalHits} results</span>
-        )}
-        <span style={{ color: "gray" }}> ordered by relevance</span>
-      </Heading>
+      <HStack mb={5} w="100%" justifyContent="flex-start" alignItems="center">
+        <Heading size="md" mb={5}>
+          Results: {totalHits}
+        </Heading>
+        <span style={{ color: "gray" }}> ordered by </span>
+        <SortingSelect
+          sortingTarget={sorting}
+          order={order}
+          onSortTargetChange={setSorting}
+          onOrderChange={setOrder}
+        />
+      </HStack>
+
       <BeerResultStack
-        beers={beers}
+        beers={localBeers}
         keywords={keywords}
         onBeerSelected={onBeerSelected}
       />
