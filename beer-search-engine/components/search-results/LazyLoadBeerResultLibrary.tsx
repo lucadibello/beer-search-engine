2462adb43a9d026ed120b816d770006aeb930875
Luca Di Bello <info@lucadibello.ch>
2023-12-06 21:29:58 +0000

    Implemented details button

--- beer-search-engine/components/search-results/LazyLoadBeerResultLibrary.tsx
+++ beer-search-engine/components/search-results/LazyLoadBeerResultLibrary.tsx
@@ -1,11 +1,13 @@
 "use client"
 
 import { searchBeer } from "@/service"
-import { SearchApiResponse } from "@/service/beer-service"
+import { Beer, SearchApiResponse } from "@/service/beer-service"
 import { getKeywords } from "@/util/keywords"
 import { WrapPromiseResult, wrapPromise } from "@/util/promises"
 import { useState, useEffect } from "react"
 import BeerResultLibrary from "./BeerResultLibrary"
+import { Portal, useDisclosure } from "@chakra-ui/react"
+import BeerDetailsModal from "./BeerDetailsModal"
 
 const useBeerSearcher = (query: string) => {
   const [beers, setBeers] =
@@ -22,6 +24,8 @@ const useBeerSearcher = (query: string) => {
 export default function LazyBeerResultLibrary({ query }: { query: string }) {
   // Use hook to search for beers
   const beers = useBeerSearcher(query)
+  const { isOpen, onClose, onOpen } = useDisclosure()
+  const [selectedBeer, setSelectedBeer] = useState<Beer | null>(null)
 
   // Do not show anything until a result is actually available
   if (!beers) {
@@ -30,13 +34,25 @@ export default function LazyBeerResultLibrary({ query }: { query: string }) {
 
   // Return the page
   return (
-    <BeerResultLibrary
-      totalHits={beers?.total_hits || 0}
-      beers={beers?.data || []}
-      keywords={getKeywords(query)}
-      onBeerSelected={(beer) => {
-        console.log("beer selected", beer)
-      }}
-    />
+    <>
+      <BeerResultLibrary
+        totalHits={beers?.total_hits || 0}
+        beers={beers?.data || []}
+        keywords={getKeywords(query)}
+        onBeerSelected={(beer) => {
+          setSelectedBeer(beer)
+          onOpen()
+        }}
+      />
+      <Portal>
+        {selectedBeer && (
+          <BeerDetailsModal
+            beer={selectedBeer}
+            isOpen={isOpen}
+            onClose={onClose}
+          />
+        )}
+      </Portal>
+    </>
   )
 }
