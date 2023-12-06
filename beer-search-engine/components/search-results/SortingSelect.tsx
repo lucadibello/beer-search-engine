eede7269d9c7436974e21b3c02d77d43f71ca6f3
Luca Di Bello <info@lucadibello.ch>
2023-12-06 14:34:48 +0000

    Created SortingSelect component

---
+++ beer-search-engine/components/search-results/SortingSelect.tsx
@@ -0,0 +1,67 @@
+import {
+  BeerSortingTarget,
+  SortOrder,
+  getSortOrders,
+  getSortingTargets,
+} from "@/util/sorter"
+import { HStack, Select } from "@chakra-ui/react"
+
+interface SortingSelectProps {
+  sortingTarget: BeerSortingTarget | "relevance"
+  order: SortOrder
+  onSortTargetChange?: (type: BeerSortingTarget) => void
+  onOrderChange?: (order: SortOrder) => void
+}
+
+export default function SortingSelect({
+  sortingTarget,
+  order,
+  onSortTargetChange,
+  onOrderChange,
+}: SortingSelectProps) {
+  // Return the component
+  return (
+    <HStack spacing={5}>
+      <Select
+        value={sortingTarget}
+        color={"gray"}
+        onChange={(e) =>
+          onSortTargetChange &&
+          onSortTargetChange(e.target.value as BeerSortingTarget)
+        }
+        size="sm"
+        variant="unstyled"
+      >
+        <option value="relevance">relevance</option>
+        {getSortingTargets().map((key) => {
+          return (
+            <option key={key} value={key}>
+              {key}
+            </option>
+          )
+        })}
+      </Select>
+
+      {sortingTarget !== "relevance" && (
+        <Select
+          value={order}
+          color={"gray"}
+          onChange={(e) =>
+            onOrderChange && onOrderChange(e.target.value as SortOrder)
+          }
+          size="sm"
+          variant="unstyled"
+        >
+          {/* Map SortOrder enum values to options */}
+          {getSortOrders().map((key) => {
+            return (
+              <option key={key} value={key}>
+                {key}
+              </option>
+            )
+          })}
+        </Select>
+      )}
+    </HStack>
+  )
+}
