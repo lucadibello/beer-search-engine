e2758a57c35824ba7f25cf3b8a1a88427779bccc
Luca Di Bello <lucadibello@proton.me>
2023-12-09 11:53:47 +0000

    Fixed format

--- backend/modules/response.py
+++ backend/modules/response.py
@@ -1,8 +1,10 @@
 from typing import List, Dict, Hashable, Any
 
 
-def format_response(data: List[Dict[Hashable, Any]], total_hits: int) -> dict:
+def format_response(data: List[Dict[Hashable, Any]], total_hits: int, additional_data: Dict[Hashable, Any] = None):
     return {
         'total_hits': total_hits,
-        'data': data
+        'data': data,
+        # Spread additional data
+        **additional_data
     }
