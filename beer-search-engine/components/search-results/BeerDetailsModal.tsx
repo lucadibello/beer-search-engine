2756e616bb1c9fb8eff2c973be6993ffef3081e3
Luca Di Bello <lucadibello@proton.me>
2023-12-09 15:06:24 +0000

    Added description to modal

--- beer-search-engine/components/search-results/BeerDetailsModal.tsx
+++ beer-search-engine/components/search-results/BeerDetailsModal.tsx
@@ -6,16 +6,38 @@ import {
   ModalHeader,
   ModalCloseButton,
   ModalBody,
-  UnorderedList,
-  ListItem,
   ModalFooter,
   Button,
+  HStack,
+  Icon,
+  Text,
+  VStack,
+  Textarea,
 } from "@chakra-ui/react"
+import { IconType } from "react-icons"
+import { FiBox, FiDollarSign, FiInfo } from "react-icons/fi"
 
 interface BeerDetailsModalProps {
   beer: Beer
   isOpen: boolean
   onClose: () => void
+}
+
+function ItemWithIcon({
+  icon,
+  children,
+}: {
+  icon: IconType
+  children: string | string[]
+}) {
+  return (
+    <HStack spacing={2}>
+      <Icon as={icon} />
+      <Text fontSize="sm" fontWeight="bold">
+        {children}
+      </Text>
+    </HStack>
+  )
 }
 
 export default function BeerDetailsModal({
@@ -28,18 +50,29 @@ export default function BeerDetailsModal({
       ? `${beer.price?.amount} ${beer.price?.currency}`
       : "Unknown"
   return (
-    <Modal isOpen={isOpen} onClose={onClose}>
+    <Modal isOpen={isOpen} onClose={onClose} size="xl">
       <ModalOverlay />
       <ModalContent>
         <ModalHeader>{beer?.name || "Details"}</ModalHeader>
         <ModalCloseButton />
         <ModalBody>
-          <UnorderedList>
-            <ListItem>Style: {beer?.style || "Unknown"}</ListItem>
-            <ListItem>Packaging: {beer?.packaging || "Unknown"}</ListItem>
-            <ListItem>Closure: {beer?.closure || "Unknown"}</ListItem>
-            <ListItem>Price: {price}</ListItem>
-          </UnorderedList>
+          <VStack spacing={2} mb={2} justify={"flex-start"} alignItems={"left"}>
+            <ItemWithIcon icon={FiInfo}>
+              Style: {beer?.style || "Unknown"}
+            </ItemWithIcon>
+            <ItemWithIcon icon={FiBox}>
+              Packaging: {beer?.packaging || "Unknown"}
+            </ItemWithIcon>
+            <ItemWithIcon icon={FiBox}>
+              Closure: {beer?.closure || "Unknown"}
+            </ItemWithIcon>
+            <ItemWithIcon icon={FiDollarSign}>Price: {price}</ItemWithIcon>
+            {/* Text area for description */}
+            <Text fontSize="sm" fontWeight="bold">
+              Description:
+            </Text>
+            <Textarea value={beer?.description || "Unknown"} minH={100} />
+          </VStack>
         </ModalBody>
 
         <ModalFooter>
